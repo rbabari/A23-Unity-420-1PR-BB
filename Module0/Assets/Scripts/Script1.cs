0f0c4863df30384da81ee4d98f7e79b7715ad3be
rbabari <r.babari@gmail.com>
2023-08-25 17:45:18 -0400

    Debug.log
    Debug.log

--- Module0/Assets/Scripts/Script1.cs
+++ Module0/Assets/Scripts/Script1.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Script1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Félicitation pour mon premier script");
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
